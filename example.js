152719078e45e597a47c98995bdab7c8ca8dc519
Gian-Luca Frei <gianlucafrei@gmail.com>
2019-04-03 12:39:46 +0200

    Added example to show how to use the lib

---
+++ example.js
@@ -0,0 +1,33 @@
+// Install package with 'npm install gianlucafrei/MiniCertificates'
+// Import the protocol library and init on curve p192
+var loac = require('loacprotocol')('p192');
+
+// Create the different parties for the demo
+let ia = new loac.IdentityAuthority(1000);
+let pa = new loac.PermissionAuthority();
+let user1 = new loac.Subject();
+let user2 = new loac.Subject();
+let resource = new loac.Resource("resource1", [ia.pk], [pa.pk], 20);
+
+// Onboarding of both users
+let req1 = user1.generateOnboardingRequest('user1');
+let req2 = user2.generateOnboardingRequest('user2');
+let c1 = ia.handleOnboaradingRequest(req1, 'user1');
+let c2 = ia.handleOnboaradingRequest(req2, 'user2');
+
+// Root token and delegation
+let now = loac.utils.dateToUnixTime(new Date());
+let t1 = pa.issueToken('user1', true, "resource1", now-1000, now+1000);
+let t2 = user1.issueDelegatedToken('user2', false, "resource1", now-500, now+500);
+
+// Access Request
+let req = user2.createAccessRequest('user2', 'open', [t1, t2], [c1, c2]);
+let serializedReq = req.serialize();
+
+console.log("Length of request: " + serializedReq.length + " bytes");
+
+let callback = function(){
+    console.log("Success");
+}
+
+resource.checkAccessRequest(serializedReq, callback)
